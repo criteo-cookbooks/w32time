62a7a0bf811b6981b1ba4b39c39b521f94057b54
tionebsalocin <nicolas.benoit@free.fr>
2016-12-06 04:30:06 +0100

    Manage several ntp servers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-#ntp server name (single)
-default['w32time']['ntpserver'] = 'pool.ntp.org'
+#ntp server names
+default['w32time']['servers'] = %w(0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org)
 default['w32time']['type'] = 'NTP'
 # 0x8 stands for time client
 default['w32time']['flag'] = '0x8'
