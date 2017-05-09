b9f90e0c01267c4b0352c642170b3d61f5ca86ff
Grégoire Seux <g.seux@criteo.com>
2017-05-09 18:02:25 +0200

    Be compatible with old chef versions
    Change-Id: I23c888da6f404c7107377cdc553277b6b9b9b320

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sre-core-infra@criteo.com'
 license          'Apache 2.0'
 description      'This cookbook configure w32time'
 long_description 'Installs/Configures w32time'
-issues_url       'https://github.com/criteo-cookbooks/w32time'
-source_url       'https://github.com/criteo-cookbooks/w32time'
+issues_url       'https://github.com/criteo-cookbooks/w32time' if respond_to? :issues_url
+source_url       'https://github.com/criteo-cookbooks/w32time' if respond_to? :source_url
 version          '0.1.0'
 supports         'windows'
