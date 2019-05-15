f993aa65fd81f92acd3f6366e57d9983f27a48c6
Baptiste Courtois <b.courtois@criteo.com>
2019-05-15 08:26:19 +0200

    Fix foodcritic violations in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name             'w32time'
 maintainer       'Nicolas BENOIT'
 maintainer_email 'sre-core-infra@criteo.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'This cookbook configure w32time'
 long_description 'Installs/Configures w32time'
+chef_version     '>= 12.16' if respond_to?(:chef_version)
 issues_url       'https://github.com/criteo-cookbooks/w32time' if respond_to? :issues_url
 source_url       'https://github.com/criteo-cookbooks/w32time' if respond_to? :source_url
 version          '0.1.0'
